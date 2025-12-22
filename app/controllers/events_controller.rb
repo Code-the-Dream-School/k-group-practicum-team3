a132e07282cd138a51f981a34812a0123b999095
vsviryd7 <ol.sviridenko777@gmail.com>
2025-12-22 14:25:57 -0500

    add show.html.erb page for tiket 3-59

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,7 @@ class EventsController < ApplicationController
   def index
     @events = Event.includes(:organizer).order(starts_at: :asc)
   end
+  def show
+    @event = Event.find(params[:id])
+  end
 end
