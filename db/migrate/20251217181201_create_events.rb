23e5fe01fb185379e029d7340b59c72628a4502b
Darya <daryapogas@gmail.com>
2025-12-22 12:13:14 -0600

    Update events schema

--- db/migrate/20251217181201_create_events.rb
+++ db/migrate/20251217181201_create_events.rb
@@ -3,15 +3,26 @@ class CreateEvents < ActiveRecord::Migration[7.2]
     create_table :events do |t|
       t.string :title, null: false
       t.text :description
+
+      t.string :location
       t.string :city
-      t.integer :price
+      t.string :state
+
       t.datetime :starts_at, null: false
       t.datetime :ends_at
+
       t.integer :category, null: false
-      t.integer :age_min
-      t.integer :age_max
+      t.integer :price
+
+      t.integer :min_age
+      t.integer :max_age
       t.integer :allowed_gender,  null: false, default: 0
-      t.references :organizer, null: false, foreign_key: { to_table: :users }
+
+      t.integer :rsvp, null: false, default: 0
+      t.boolean :accessible, default: false
+      t.integer :max_capacity
+
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
