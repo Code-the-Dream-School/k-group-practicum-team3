83179e5f755b1a0fb0536fce9563c674e2c10663
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2025-12-23 15:14:33 -0700

    PR test fixes

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,16 +1,16 @@
 module UserHelper
     def organizer?(user)
         if user.has_role? :organizer
-            return true
+            true
         else
-            return false
+            false
         end
     end
 
   # Returns true if the user has the student role.
   def student?(user)
     if user.has_role? :student
-      return true
+      true
     else
       false
     end
@@ -19,7 +19,7 @@ module UserHelper
   # Returns true if the user has the parent role.
   def parent?(user)
     if user.has_role? :parent
-      return true
+      true
     else
       false
     end
@@ -28,24 +28,24 @@ module UserHelper
   # Returns true if the user has the admin role.
   def admin?(user)
     if user.has_role? :admin
-      return true
+      true
     else
-      return false
+      false
     end
   end
 
   # Returns a human-friendly version of the user’s role (e.g. “Organizer”).
   def display_role(user)
     if admin?(user)
-      return "Admin"
+      "Admin"
     elsif organizer?(user)
-      return "Organizer"
+       "Organizer"
     elsif parent?(user)
-      return "Parent"
+      "Parent"
     elsif student?(user)
-      return "Student"
+      "Student"
     else
-      return ""
+      ""
     end
   end
 
@@ -55,47 +55,46 @@ module UserHelper
       return true       
     end
     return false
-
   end
 
   # Returns true if the user is allowed to create new events.
   def can_create_events?(user)
     if user.has_role? :organizer
-      return true
+      true
     end
-    return false
+    false
   end
 
   # Returns the correct dashboard path based on the user’s role.
   def dashboard_path_for(user)
     # TODO: No dashboard yet
-    return "TODO: dashboard_path(user)"
+    "TODO: dashboard_path(user)"
   end
 
   # Returns a list of navigation links appropriate to the user’s role.
   def navigation_links_for(user)
-    #TODO: No navigation pages yet
+    # TODO: No navigation pages yet
     if admin?(user)
-        return "TODO: navigation_links_for(user)"
+        "TODO: navigation_links_for(user)"
     elsif organizer?(user)
-        return "TODO: navigation_links_for(user)"
+        "TODO: navigation_links_for(user)"
     elsif parent?(user)
-        return "TODO: navigation_links_for(user)"
+        "TODO: navigation_links_for(user)"
     elsif student?(user)
-        return "TODO: navigation_links_for(user)"
+        "TODO: navigation_links_for(user)"
     else
-        return "TODO: navigation_links_for(user)"
+        "TODO: navigation_links_for(user)"
     end
   end
 
   # Returns the best name to show for a user (name or email fallback).
   def user_display_name(user)
     if user.name.present?
-      return user.name
+      user.name
     elsif user.email.present?
-      return user.email
+      user.email
     else
-      return ""
+      ""
     end
   end
 
