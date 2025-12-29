b1a5cbb1ba588ef5900311280dc080d350249ead
vsviryd7 <ol.sviridenko777@gmail.com>
2025-12-29 14:27:13 -0500

    modify index.html

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.includes(:organizer).order(starts_at: :asc)
+    @events = Event.includes(:user).order(starts_at: :asc)
   end
   def show
     @event = Event.find(params[:id])
