0babc711e18b71c70e4e21d7c173bee137b5947e
StealthyScripter <wendotbrian@gmail.com>
2026-01-02 11:38:55 -0500

    Adjusted the code and test files to pass the rspec

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,39 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.create!(
+      first_name: "John",
+      last_name: "Doe",
+      email: "john@example.com",
+      gender: :male,
+      password: "password",
+      password_confirmation: "password",
+      city: "Some City",
+      state: "Some State",
+      zip: "12345"
+    )
+  end
+
+  describe "#user_display_name" do
+    it "returns the name if present" do
+      expect(helper.user_display_name(user)).to eq("John Doe")
+    end
+
+    it "falls back to email if name is missing" do
+      user.update(first_name: nil, last_name: nil)
+      expect(helper.user_display_name(user)).to eq("john@example.com")
+    end
+  end
+
+  describe "#display_role" do
+    it "returns Admin when user has admin role" do
+      user.add_role :admin
+      expect(helper.display_role(user)).to eq("Admin")
+    end
+
+    it "returns empty string if user has no role" do
+      expect(helper.display_role(user)).to eq("")
+    end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:user) { User.create!(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password", password_confirmation: "password", location_type: :online, gender: :male) }
+
+  before do
+    sign_in user
+  end
+
   describe "GET /show" do
     it "returns http success" do
-      get "/users/show"
+      get user_path(user)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /edit" do
     it "returns http success" do
-      get "/users/edit"
+      get edit_user_registration_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
