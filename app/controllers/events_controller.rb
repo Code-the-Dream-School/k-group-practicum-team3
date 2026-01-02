f6958ab43392e23139002b15432cb6c956a01b5c
vsviryd7 <ol.sviridenko777@gmail.com>
2026-01-01 22:12:30 -0500

    merge coflict

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,14 +4,13 @@ class EventsController < ApplicationController
   end
   def show
     @event = Event.find(params[:id])
-  end  
+  end
   def new
     @event = Event.new
   end
 
   def create
     @event = Event.new
-
     redirect_to new_event_path, notice: "Event submitted"
   end
 end
