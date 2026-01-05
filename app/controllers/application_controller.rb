5dbb20398a2ec843b19b2a9cad16438a9fbb782b
StealthyScripter <wendotbrian@gmail.com>
2026-01-04 21:13:18 -0500

    Added a profile edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include Pundit::Authorization
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [
+      :first_name, :last_name, :age, :gender, :bio,
+      :city, :state, :zip, :location_type,
+      :profile_picture
+    ]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+  end
 end
