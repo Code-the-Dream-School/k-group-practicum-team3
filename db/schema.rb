5b9301af5307da67819be747aad1717113108cd5
StealthyScripter <wendotbrian@gmail.com>
2026-01-06 22:51:02 -0500

    Updated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,16 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_02_032134) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "enrollments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_enrollments_on_event_id"
+    t.index ["user_id", "event_id"], name: "index_enrollments_on_user_id_and_event_id", unique: true
+    t.index ["user_id"], name: "index_enrollments_on_user_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
@@ -96,8 +106,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_02_032134) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "enrollments", "events"
-  add_foreign_key "enrollments", "users"
   create_table "users_roles", id: false, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "role_id", null: false
@@ -108,6 +116,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_02_032134) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "enrollments", "events"
+  add_foreign_key "enrollments", "users"
   add_foreign_key "events", "users"
   add_foreign_key "users_roles", "roles"
   add_foreign_key "users_roles", "users"
