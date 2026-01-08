fbce5df006762d56b06dbf4be6ad7f272f1b274e
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-08 16:43:06 -0700

    Renamed files to events, merged with new commits, added basic functionality to new, edit, show, and index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,17 @@
 class EventsController < ApplicationController
+  
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+  end
+
   def new
-    #
   end
 
   def create
