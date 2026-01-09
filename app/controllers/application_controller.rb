bfb2d8ba326319b507ecba08045d2071dbf9a4fa
StealthyScripter <wendotbrian@gmail.com>
2026-01-09 11:26:51 -0500

    Fixed rubocop issue in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
 
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+  end
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
