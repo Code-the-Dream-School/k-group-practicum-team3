4c53c0502bea16d9d7b4c67020035e8d57ca2c40
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-08 20:26:58 -0700

    With dummy parameter, added a dashboard index view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
     # Displays only the events associated with the current user
     def index
-        @events = Event.where(params(current_user.id))
+        # TODO: Uncomment when user can be signed in
+        #@events = Event.where(params(current_user.id))
+        @events = Event.where(user_id: 1)
     end
 end
