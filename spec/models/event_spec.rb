41a64b050f4f29cb816a212130cb00ab2d472084
vsviryd7 <ol.sviridenko777@gmail.com>
2026-01-08 22:20:29 -0500

    test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,37 +1,5 @@
 require "rails_helper"
 
-RSpec.describe "Events", type: :request do
-  let(:user) do
-    User.create!(
-      email: "organizer@example.com",
-      password: "password123",
-      first_name: "Test",
-      last_name: "Organizer",
-      location_type: :online
-    )
-  end
-
-  let(:event) do
-    Event.create!(
-      title: "Soccer",
-      category: :sports,
-      starts_at: 1.day.from_now,
-      allowed_gender: :any,
-      rsvp: :public_event,
-      user: user
-    )
-  end
-
-  it "renders the index page" do
-    get events_path
-    expect(response).to have_http_status(:ok)
-  end
-
-  it "renders the show page" do
-    get event_path(event)
-    expect(response).to have_http_status(:ok)
-  end
-
 RSpec.describe Event, type: :model do
    describe "date/time edge cases" do
     let(:user) do
@@ -89,5 +57,4 @@ RSpec.describe Event, type: :model do
       expect(event).to be_valid
     end
   end
-end
 end
