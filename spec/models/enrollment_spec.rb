fafeff6b19e08fc1439cba664d095db668332155
vsviryd7 <ol.sviridenko777@gmail.com>
2026-01-09 12:56:25 -0500

    test file

--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
+require "rails_helper"
+
 RSpec.describe Enrollment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:organizer) do
+    User.create!(
+      email: "org@example.com",
+      password: "test123",
+      first_name: "Org",
+      last_name: "User"
+    )
+  end
+
+  let!(:user) do
+    User.create!(
+      email: "user@example.com",
+      password: "test123",
+      first_name: "Test",
+      last_name: "User"
+    )
+  end
+
+  let!(:event) do
+    attrs = { user: organizer, title: "Test Event", category: :other }
+    attrs[:starts_at] = 1.day.from_now if Event.new.respond_to?(:starts_at)
+    Event.create!(attrs)
+  end
+
+  it "is valid with a user and an event" do
+    enrollment = described_class.new(user: user, event: event)
+    expect(enrollment).to be_valid
+  end
 end
