7992df4898a41e964bc7e35982e3efbcf99df07f
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-09 17:46:32 -0700

    saving changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
-    #before_action :authenticate_user!
+    before_action :authenticate_user!
     # Displays only the events associated with the current user
     def index
         # TODO: Uncomment when user can be signed in
-        #@events = Event.where(params(current_user.id))
-        @events = Event.where(user_id: 1)
+        @events = Event.where(params(current_user.id))
+        #@events = Event.where(user_id: 1)
     end
 end
