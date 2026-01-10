16e9fb68cc6383841a4aa5e44453c0e17c921359
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-10 00:47:47 -0700

    Lists all events, combined with Olga's table for the event index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     # Displays only the events associated with the current user
     def index
-        # TODO: Uncomment when user can be signed in
-        @events = Event.where(params(current_user.id))
-        #@events = Event.where(user_id: 1)
+        @events = current_user.events
+        #placeholder id for lack of a sign up functionality
+        #@events = Event.where(user_id:2)
     end
 end
