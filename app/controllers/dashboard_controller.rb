6ca2041a63ea86efab6790f347fba6c05355b52a
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-11 23:02:19 -0700

    Fixed rubocop violations

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     # Displays only the events associated with the current user
     def index
-        #@events = current_user.events
+        # @events = current_user.events
         @events = Event.where(id: current_user.id)
     end
 end
