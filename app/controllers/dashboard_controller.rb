1ca4825d7348b0373979fefc169359706eb0979d
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-11 23:00:45 -0700

    Added edit and delete buttons with appropriate pathing (needs CRUD backend)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,7 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     # Displays only the events associated with the current user
     def index
-        @events = current_user.events
-        #placeholder id for lack of a sign up functionality
-        #@events = Event.where(user_id:2)
+        #@events = current_user.events
+        @events = Event.where(id: current_user.id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,6 +14,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :events, only: [ :new, :create ]
   root "posts#index"
 end
