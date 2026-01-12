cbdc3a2dc16e5dde1b398f651714a0d964b1f853
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-12 00:12:25 -0700

    Added TODO for the joins table and fixed minor bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,8 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     # Displays only the events associated with the current user
     def index
-        if organizer?(current_user)
-            @events = Event.where(id: current_user.id)
-        else
-            redirect_to root_path, notice: "You do not have permission to view that page."
-        end
+        #TODO: Needs joins table for registrations to display more than one event
+        #@events = current_user.events
+        @events = Event.where(user_id: current_user.id)
     end
 end
