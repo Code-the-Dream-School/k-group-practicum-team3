29610f1d9acbca5c8dcc0308fa3b273788745eae
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2026-01-11 23:28:07 -0700

    Limited index to organizers and redirects to root path

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,10 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     # Displays only the events associated with the current user
     def index
-        # @events = current_user.events
-        @events = Event.where(id: current_user.id)
+        if organizer?(current_user)
+            @events = Event.where(id: current_user.id)
+        else
+            redirect_to root_path, notice: "You do not have permission to view that page."
+        end
     end
 end
