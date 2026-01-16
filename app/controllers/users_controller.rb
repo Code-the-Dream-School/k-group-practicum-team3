17b4deeb35bd23c24bb93e9115335cda63d04a7d
StealthyScripter <wendotbrian@gmail.com>
2026-01-16 00:03:01 -0500

    Created the Users index page to display all the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
   def show
-    @user = current_user
+    @user = User.find(params[:id])
   end
 end
