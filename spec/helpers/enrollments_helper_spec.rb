9fd79a006e8683aba273a2778fccbfcd59edc60e
vsviryd7 <ol.sviridenko777@gmail.com>
2026-01-15 23:39:02 -0500

    merge coflict

--- spec/helpers/enrollments_helper_spec.rb
+++ spec/helpers/enrollments_helper_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe EnrollmentsHelper, type: :helper do
   let!(:organizer) { User.create!(email: "org@example.com", password: "test123", first_name: "org",
-    last_name: "lastname") }
-  let!(:user)      { User.create!(email: "user@example.com", password: "test123", first_name: "firstname",
-    last_name: "lastname") }
+    last_name: "lastname", city: "Chicago", state: "IL", zip: "60601", location_type: :online) }
+  let!(:user) { User.create!(email: "user@example.com", password: "test123", first_name: "firstname",
+    last_name: "lastname", city: "Chicago", state: "IL", zip: "60601", location_type: :online) }
 
   let!(:event) do
     attrs = { user: organizer,
@@ -49,14 +49,30 @@ RSpec.describe EnrollmentsHelper, type: :helper do
       event.update!(max_capacity: 1)
       Enrollment.create!(event: event, user: user)
 
-      another_user = User.create!(email: "u2@example.com", password: "password123", first_name: "U2",
-  last_name: "User")
+      another_user = User.create!(
+        email: "u2@example.com",
+        password: "password123",
+        first_name: "U2",
+        last_name: "User",
+        city: "Chicago",
+        state: "IL",
+        zip: "60601",
+        location_type: :online
+      )
       expect(helper.can_enroll?(event, another_user)).to be(false)
     end
 
     it "returns true when user is not organizer, not enrolled, and event not full" do
-      another_user = User.create!(email: "u3@example.com", password: "password123", first_name: "U2",
-  last_name: "User")
+      another_user = User.create!(
+        email: "u2@example.com",
+        password: "password123",
+        first_name: "U2",
+        last_name: "User",
+        city: "Chicago",
+        state: "IL",
+        zip: "60601",
+        location_type: :online
+      )
       expect(helper.can_enroll?(event, another_user)).to be(true)
     end
   end
