f86a03f32c759f3af23863fdf76b1bc2e91999b9
KJ Loving <kjloving1400@gmail.com>
2026-01-22 18:02:50 -0800

    fixed rubocop flag

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
-  
+
   def index
     @events = Event.includes(:user).order(starts_at: :asc)
   end
