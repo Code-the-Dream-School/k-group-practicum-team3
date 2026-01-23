d14534ebe6fb3dd8ec62030cc0bab3e9daefd7ac
StealthyScripter <wendotbrian@gmail.com>
2026-01-23 13:25:02 -0500

    Removed the duplicate code. It now contains the requested role param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,25 +25,4 @@ class ApplicationController < ActionController::Base
     redirect_to(root_path, alert: "You are not authorized to perform this action.")
     before_action :configure_permitted_parameters, if: :devise_controller?
   end
-
-  protected
-
-  def configure_permitted_parameters
-    added_attrs = [
-      :first_name, :last_name, :age, :gender, :bio,
-      :city, :state, :zip, :location_type,
-      :profile_picture
-    ]
-
-    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
-    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
-  end
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private
-
-  def user_not_authorized
-    redirect_to(root_path, alert: "You are not authorized to perform this action.")
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_08_023743) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_22_235959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,11 +98,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_08_023743) do
     t.integer "age"
     t.string "phone"
     t.text "bio"
+    t.integer "location_type", default: 0
     t.integer "gender"
     t.string "city"
     t.string "state"
     t.string "zip"
-    t.integer "location_type", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
