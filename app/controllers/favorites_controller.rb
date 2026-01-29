b1aab3cb14ebc11f2c390e81ae8bc8f659fddbde
vsviryd7 <ol.sviridenko777@gmail.com>
2026-01-29 13:52:41 -0500

    add model favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,24 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_event, only: [:create, :destroy]
+
+  def index
+    @events = current_user.favorited_events
+  end
+
+  def create
+    current_user.favorites.create!(event: @event)
+    redirect_back fallback_location: events_path
+  end
+
+  def destroy
+    current_user.favorites.find_by(event: @event)&.destroy
+    redirect_back fallback_location: events_path
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+end
