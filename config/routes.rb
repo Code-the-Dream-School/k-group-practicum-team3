73e2ce917a36322f3d4794b53e124c75f5856096
SingLZ <0lix.ing0@gmail.com>
2026-01-30 12:55:16 -0500

    change update profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  registrations: "users/registrations"
+  }
   resources :users, only: [ :index, :show ], controller: "users"
   resources :events do
     resources :enrollments, only: [ :create, :destroy ]
