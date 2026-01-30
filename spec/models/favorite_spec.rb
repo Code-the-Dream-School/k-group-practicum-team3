a9e1b98a3c080ba8bc077908454da2d893535b06
vsviryd7 <ol.sviridenko777@gmail.com>
2026-01-29 23:19:27 -0500

    add test and favorites/index. html page save favorites

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it "belongs to a user" do
+      assoc = described_class.reflect_on_association(:user)
+      expect(assoc.macro).to eq(:belongs_to)
+    end
+
+    it "belongs to an event" do
+      assoc = described_class.reflect_on_association(:event)
+      expect(assoc.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe "validations" do
+    let(:user) { create(:user) }
+    let(:event) { create(:event) }
+
+    before do
+      create(:favorite, user: user, event: event)
+    end
+
+    it "does not allow duplicate favorites" do
+      duplicate = build(:favorite, user: user, event: event)
+      expect(duplicate).not_to be_valid
+    end
+
+    it "adds an error when duplicate favorite is attempted" do
+      duplicate = build(:favorite, user: user, event: event)
+      duplicate.validate
+      expect(duplicate.errors[:user_id]).to include("has already been taken")
+    end
+  end
 end
