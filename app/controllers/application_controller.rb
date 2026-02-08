5b6be5fbcb8e56f75515ebe42a8f15217285aad5
Darya <daryapogas@gmail.com>
2026-02-08 14:33:59 -0600

    Fix unauthorized redirect and improve event edit UX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    redirect_to(root_path, alert: "You are not authorized to perform this action.")
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    redirect_to(dashboard_path, alert: "You are not authorized to perform this action.")
   end
 
   def after_sign_in_path_for(_resource)
